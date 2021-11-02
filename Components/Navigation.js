fe6a9a7216876dcb40c3b559008587abfe6612f1
smar152 <moody.butterflies@gmail.com>
2021-11-02 12:15:19 +0200

    first commit

---
+++ Components/Navigation.js
@@ -0,0 +1,7 @@
+export default function Navigation(props) {
+  return (
+    <div>
+      <span>Home</span> | <span>News</span>
+    </div>
+  );
+}
---
+++ Components/Search.js
@@ -0,0 +1,7 @@
+export default function Search(props) {
+  return (
+    <div>
+      <p>Search</p>
+    </div>
+  );
+}
